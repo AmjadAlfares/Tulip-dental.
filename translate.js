2d567cba981aa10fdc04d55f7ec9c9ed840a064a
AmjadAlfares <amjadmamdouhalfares@gmail.com>
2025-05-10 17:13:19 +0200

    Delete translate.js

--- translate.js
+++
@@ -1,9 +0,0 @@
-function setLanguage(lang) {
-  fetch(`lang/${lang}.json`)
-    .then(res => res.json())
-    .then(data => {
-      document.getElementById("title").textContent = data.title;
-      document.getElementById("about").textContent = data.about;
-      document.getElementById("contact").textContent = data.contact;
-    });
-}
